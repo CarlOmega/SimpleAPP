197fb6e866ae28510437654a5755ee8805178712
Carl Humphries <carl.w.humphries@gmail.com>
2021-06-22 14:21:47 +1000

    styl: welcome screen
    Using UI kitten to save time on UI.

--- App.tsx
+++ App.tsx
@@ -4,24 +4,28 @@ import HomeNavigator from '@navigation/HomeNavigator';
 import { AuthProvider, AuthContext } from '@states/AuthContext';
 import AuthNavigator from '@navigation/AuthNavigator';
 import { ActivityIndicator } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider } from '@ui-kitten/components';
 
 const App = () => {
 
   return (
-    <AuthProvider>
-      <NavigationController>
-        <AuthContext.Consumer>
-          {({user, isLoading}) => 
-            isLoading ? <ActivityIndicator /> :
-            !user 
-            ?
-            <AuthNavigator />
-            :
-            <HomeNavigator />
-          }
-        </AuthContext.Consumer>
-      </NavigationController>
-    </AuthProvider>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <AuthProvider>
+        <NavigationController>
+          <AuthContext.Consumer>
+            {({user, isLoading}) => 
+              isLoading ? <ActivityIndicator /> :
+              !user 
+              ?
+              <AuthNavigator />
+              :
+              <HomeNavigator />
+            }
+          </AuthContext.Consumer>
+        </NavigationController>
+      </AuthProvider>
+    </ApplicationProvider>
   );
 };
 
--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,14 +1,23 @@
+import { Layout, Text, Button } from '@ui-kitten/components';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, Button } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 const WelcomeScreen = ({navigation, route}: any) => {
 
   return (
     <SafeAreaView style={styles.screen}>
-      <Text style={styles.text}>Welcome</Text>
-      <Button title={"Owner Sign up"} onPress={() => navigation.navigate("Signup", {type: "owner"})}/>
-      <Button title={"User Sign up"} onPress={() => navigation.navigate("Signup", {type: "user"})}/>
-      <Button title={"Have an account? Sign in"} onPress={() => navigation.navigate("Signin")}/>
+      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text category='h1' style={{marginBottom: 100}}>Welcome</Text>
+        <Button style={styles.button} appearance='outline' onPress={() => navigation.navigate("Signup", {type: "owner"})}>
+          Owner Sign up
+        </Button>
+        <Button style={styles.button} appearance='outline' onPress={() => navigation.navigate("Signup", {type: "user"})}>
+          User Sign up
+        </Button>
+        <Text style={styles.text}>
+          Have an account? <Text style={styles.text} status='primary' onPress={() => navigation.navigate("Signin")}>Sign in</Text>
+        </Text>
+      </Layout>
     </SafeAreaView>
   )
 }
@@ -17,9 +26,13 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1
   },
+  button: {
+    margin: 5,
+    borderRadius: 15,
+    width: "80%"
+  },
   text: {
     fontFamily: "Roboto",
-    fontSize: 20,
     textAlign: "center"
   }
 })
